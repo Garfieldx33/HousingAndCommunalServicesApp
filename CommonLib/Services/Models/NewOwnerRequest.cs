779035849433300759cb129a1c9a2729b1c744b2
veratrofimova <kuzik-86@mail.ru>
2024-01-18 01:49:37 +0300

    Создан сервис жителя

---
+++ CommonLib/Services/Models/NewOwnerRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib.Services.Models
+{
+    public class NewOwnerRequest
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public string BirthDate { get; set; }
+        public string Address { get; set; }
+        public string Inn { get; set; }
+        public string Snils { get; set; }
+    }
+}
---
+++ CommonLib/Services/Models/OwnerDataResponce.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLib.Services.Models
+{
+    public class OwnerDataResponce
+    {
+        public string Fio { get; set; }
+        public string Address { get; set; }
+        public decimal Debt { get; set; }
+    }
+}
