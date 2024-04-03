2abc8322aeefb4283bd50a4ef2d874852c261794
Andrey Leonchenko <aleonchenko@intas-company.com>
2024-04-03 11:14:11 +0500

    добавлен мьютекс в ConsoleUi

--- UiConsole/Program.cs
+++ UiConsole/Program.cs
@@ -5,24 +5,38 @@ using CommonLib.Enums;
 using UiConsole;
 using UiConsole.Strategy;
 using UiConsole.Strategy.StrategyImpl.UserStrategy;
-
-Console.Write("Логин: ");
-string? login = Console.ReadLine();
-Console.Write("Пароль: ");
-string? pwd = Console.ReadLine();
-
-var userResult = await CommonMethodsInvoker.GetInfoFromWebAPI<User>(
-    $@"http://192.168.50.175:7080/Users/GetUserByLoginPwd/{login}/{pwd}", HttpMethodsEnum.Get, null);
-if (userResult != null)
+Mutex mutex = new(true, "UiConsole", out bool createdNew);
+if (createdNew)
 {
-    User userInfo = userResult;
-    Console.WriteLine($"Добро пожаловать {userInfo.FirstName} {userInfo.SecondName}");
-    UserRunner userRunner = GetUserRunner(userInfo);
-    userRunner.RunUser();
+    mutex.WaitOne();
+    Console.Write("Логин: ");
+    string? login = Console.ReadLine();
+    Console.Write("Пароль: ");
+    string? pwd = Console.ReadLine();
 
+    var userResult = await CommonMethodsInvoker.GetInfoFromWebAPI<User>(
+        $@"http://192.168.50.175:7080/Users/GetUserByLoginPwd/{login}/{pwd}", HttpMethodsEnum.Get, null);
+    if (userResult != null)
+    {
+        User userInfo = userResult;
+        Console.WriteLine($"Добро пожаловать {userInfo.FirstName} {userInfo.SecondName}");
+        UserRunner userRunner = GetUserRunner(userInfo);
+        userRunner.RunUser();
+
+    }
+    Console.WriteLine("Для выхода нажмите любую клавишу");
+    Console.ReadKey();
+    mutex.ReleaseMutex();
 }
-Console.WriteLine("Для выхода нажмите любую клавишу");
-Console.ReadKey();
+else
+{
+    Console.BackgroundColor = ConsoleColor.Red;
+    Console.ForegroundColor = ConsoleColor.Black;
+    Console.WriteLine($"Невозможно запустить второй экземпляр приложения. Автоматический выход через 5 секунд");
+    Thread.Sleep(5000);
+    mutex.Dispose();
+}
+
 
 static UserRunner GetUserRunner(User userInfo)
 {
